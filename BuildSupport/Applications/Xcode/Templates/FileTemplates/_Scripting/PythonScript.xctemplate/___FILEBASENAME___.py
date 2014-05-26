f524997e14ba3c59ce3bde917bf951985c1ea37e
carolclark <carolclark@cox.net>
2014-05-25 21:25:46 -0500

    clarify file templates (#38)
    distinction between project and repository

--- BuildSupport/Applications/Xcode/Templates/FileTemplates/_Scripting/PythonScript.xctemplate/___FILEBASENAME___.py
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_Scripting/PythonScript.xctemplate/___FILEBASENAME___.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 #  ___FILENAME___
-#  <#Project#>
+#  <#Repository#>/___PROJECTNAME___
 #
 #  Created by ___FULLUSERNAME___ on ___DATE___.
 #  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
