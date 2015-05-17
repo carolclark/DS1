cdddeac5142bc6e776e22a1ed43dfc391928a3b1
carolclark <carolclark@cox.net>
2015-05-17 14:50:18 -0500

    Xcode: C++ Class template (#38)

---
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_CPlusPlus/C++ Class.xctemplate/___FILEBASENAME___.cp
@@ -0,0 +1,15 @@
+// =================================================================================
+//  ___FILENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ for project ___PROJECTNAME___.
+//	Copyright (c) ___YEAR___ C & C Software, Inc. All rights reserved.
+// =================================================================================
+
+
+#include "___FILEBASENAMEASIDENTIFIER___.h"
+
+___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___() {
+}
+
+___FILEBASENAMEASIDENTIFIER___::~___FILEBASENAMEASIDENTIFIER___() {
+}
---
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_CPlusPlus/C++ Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,19 @@
+// =================================================================================
+//  ___FILENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ for project ___PROJECTNAME___.
+//	Copyright (c) ___YEAR___ C & C Software, Inc. All rights reserved.
+// =================================================================================
+
+#pragma once
+
+/*!	<#doxygenDocs#>
+ */
+class ___FILEBASENAMEASIDENTIFIER___ {
+public:
+
+	___FILEBASENAMEASIDENTIFIER___();
+	___FILEBASENAMEASIDENTIFIER___ (const ___FILEBASENAMEASIDENTIFIER___& /*original*/) { }
+	___FILEBASENAMEASIDENTIFIER___&			operator= (const ___FILEBASENAMEASIDENTIFIER___& /*original*/) { return *this; }
+	virtual						~___FILEBASENAMEASIDENTIFIER___();
+};
