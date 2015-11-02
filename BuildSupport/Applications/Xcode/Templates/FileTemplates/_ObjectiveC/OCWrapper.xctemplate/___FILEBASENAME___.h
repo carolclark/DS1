31a7d2f1f7342c475f476f9974a72215b602496b
Carol Clark <carolclark@cox.net>
2015-11-02 15:49:59 -0600

    OCWrapper template: replace content with snippet
    (#38)

--- BuildSupport/Applications/Xcode/Templates/FileTemplates/_ObjectiveC/OCWrapper.xctemplate/___FILEBASENAME___.h
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_ObjectiveC/OCWrapper.xctemplate/___FILEBASENAME___.h
@@ -5,18 +5,5 @@
 //	Copyright (c) ___YEAR___ C & C Software, Inc. All rights reserved.
 // =================================================================================
 
-#import <objc/objc-runtime.h>
-#import "AccessorTypes.h"
-#import "___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#>.h"
-
-OBJC_CLASS (___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#>);
-
-class ___FILEBASENAMEASIDENTIFIER___ {
-
-public:
-	___FILEBASENAMEASIDENTIFIER___();
-	~___FILEBASENAMEASIDENTIFIER___();
-
-protected:
-	___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#> * wrapped;		/*!< \brief	pointer to primary (Objective-C) class */
-};
+___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#>
+<#use with snippet OCWrapper# to generate text for all three files#>
--- BuildSupport/Applications/Xcode/Templates/FileTemplates/_ObjectiveC/OCWrapper.xctemplate/___FILEBASENAME___.mm
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_ObjectiveC/OCWrapper.xctemplate/___FILEBASENAME___.mm
@@ -5,13 +5,4 @@
 //	Copyright (c) ___YEAR___ C & C Software, Inc. All rights reserved.
 // =================================================================================
 
-#import "___FILEBASENAMEASIDENTIFIER___.h"
-#import "___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#>.h"
-
-___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___()
-	: wrapped([[___FILEBASENAMEASIDENTIFIER___<#delete suffix OC#> alloc] init]) {
-}
-
-___FILEBASENAMEASIDENTIFIER___::~___FILEBASENAMEASIDENTIFIER___() {
-	[wrapped release];
-}
+<#content from snippet#>
