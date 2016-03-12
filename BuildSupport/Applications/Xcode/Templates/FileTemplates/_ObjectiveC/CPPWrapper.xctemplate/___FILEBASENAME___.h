7d89c75da3d4d12539611c95e7ec8e458afb6709
carolclark <carolclark@cox.net>
2016-03-12 12:37:20 -0600

    Revert "Revert "Merge branch 'CPPWrapperTemplate' (#38)""
    This reverts commit a73c17530825a9358d18d971c6104eda069d8104.

---
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_ObjectiveC/CPPWrapper.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,12 @@
+// =================================================================================
+//  ___FILENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___ for project ___PROJECTNAME___.
+//	Copyright (c) ___YEAR___ C & C Software, Inc. All rights reserved.
+// =================================================================================
+
+#import <Foundation/Foundation.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject
+
+@end
