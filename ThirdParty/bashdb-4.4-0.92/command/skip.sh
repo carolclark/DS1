74c3e2548737d80d5199501523129d271271971c
carolclark <carolclark@cox.net>
2016-07-29 16:48:09 -0500

    add: ThirdParty/mcpp-4.4-0.92
    switch-to-bash-#99

---
+++ ThirdParty/bashdb-4.4-0.92/command/skip.sh
@@ -0,0 +1,34 @@
+# -*- shell-script -*-
+# gdb-like "skip" (step over) commmand.
+#
+#   Copyright (C) 2010, 2011 Rocky Bernstein <rocky@gnu.org>
+#
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation; either version 2, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program; see the file COPYING.  If not, write to
+#   the Free Software Foundation, 59 Temple Place, Suite 330, Boston,
+#   MA 02111 USA.
+
+_Dbg_help_add skip \
+"skip [COUNT]
+
+Skip (don't run) the next COUNT command(s).
+
+If COUNT is given, stepping occurs that many times before
+stopping. Otherwise COUNT is one. COUNT an be an arithmetic
+expression. See also \"next\" and \"step\"."
+
+_Dbg_do_skip() {
+    _Dbg_last_cmd='skip'
+    _Dbg_next_skip_common 1 $*
+    return $?
+}
