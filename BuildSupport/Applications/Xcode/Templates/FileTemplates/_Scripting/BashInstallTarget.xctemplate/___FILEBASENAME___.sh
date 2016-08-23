bd1634fce72da18fceb08173b4872b223ca260be
carolclark <carolclark@cox.net>
2016-08-23 18:26:20 -0500

    add: Xcode file template BashInstallTarget
    Apparently, initial file of a new  project must sometimes be created by Xcode.
    setup Sharness #99

---
+++ BuildSupport/Applications/Xcode/Templates/FileTemplates/_Scripting/BashInstallTarget.xctemplate/___FILEBASENAME___.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#  ___FILENAME___
+
+# Created by ___FULLUSERNAME___ on ___DATE___ for project ___PROJECTNAME___.
+# Copyright (c) ___YEAR___ by C & C Software, Inc. All rights reserved.
+
+main() {
+}
+ 
+main "$@"
