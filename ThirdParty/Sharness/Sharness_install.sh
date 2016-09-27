318d3dc5215ada8b8c3fc8430576a48f51dab569
carolclark <carolclark@cox.net>
2016-09-27 16:58:09 -0500

    add: Sharness - project Sharness, Sharness_install
    *.fauxprod omitted for now; Test target not yet added
    shell script testing #127

---
+++ ThirdParty/Sharness/Sharness_install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#  Sharness_install.sh
+
+# Created by Carol Clark on 9/27/16 for project Sharness.
+# Copyright (c) 2016 by C & C Software, Inc. All rights reserved.
+
+USAGE='
+Sharness_install.ksh -- install Sharness shell testing system
+#	custom script to install Sharness
+'
+
+main() {
+	echo $(pwd)
+}
+ 
+main "$@"
